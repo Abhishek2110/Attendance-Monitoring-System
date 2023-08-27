f56ffbb0a65304ee7a5b7eba949f0b32d77d93ac
Abhishek Rajpal <86553778+Abhishek2110@users.noreply.github.com>
2023-08-27 18:21:23 +0530

    Update MarkAttendance.js
    commit

--- attendance_management/MarkAttendance.js
+++ attendance_management/MarkAttendance.js
@@ -1,1 +1,39 @@
+// src/components/MarkAttendance.js
+import React, { useState } from 'react';
+
+const MarkAttendance = ({ classId, students, onSubmitAttendance }) => {
+  const [attendance, setAttendance] = useState([]);
+
+  const handleAttendanceChange = (studentId) => {
+    const updatedAttendance = attendance.includes(studentId)
+      ? attendance.filter(id => id !== studentId)
+      : [...attendance, studentId];
+    setAttendance(updatedAttendance);
+  };
+
+  const submitAttendance = () => {
+    onSubmitAttendance(classId, attendance);
+  };
+
+  return (
+    <div>
+      <h2>Mark Attendance</h2>
+      <ul>
+        {students.map(student => (
+          <li key={student.id}>
+            <input
+              type="checkbox"
+              checked={attendance.includes(student.id)}
+              onChange={() => handleAttendanceChange(student.id)}
+            />
+            {student.name}
+          </li>
+        ))}
+      </ul>
+      <button onClick={submitAttendance}>Submit Attendance</button>
+    </div>
+  );
+};
+
+export default MarkAttendance;
 
